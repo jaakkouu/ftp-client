e7b6e303ebb770e71718d9ea44608f0f4bed9fc2
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-01-23 21:56:55 +0200

    added basic layout

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
 import wx
 
-app = wx.App()
-frame = wx.Frame(parent=None, title='Hello World')
-frame.Show()
-app.MainLoop()
+class ToolbarPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        box = wx.BoxSizer(wx.HORIZONTAL)
+        text = wx.StaticText(self, label="Toolbar", style=wx.ALIGN_CENTRE)
+        box.Add(text, 0, wx.EXPAND | wx.ALL, 15)
+        self.SetSizer(box);
+        self.SetBackgroundColour('#0074D9') #blue
+
+class ConsolePanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        box = wx.BoxSizer(wx.VERTICAL)
+        text = wx.StaticText(self, label="Console", style=wx.ALIGN_LEFT)
+        box.Add(text, 0, wx.EXPAND | wx.ALL, 15)
+        self.SetSizer(box);
+        self.SetBackgroundColour('#01FF70') #lime
+
+class LocalDirPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        box = wx.BoxSizer(wx.VERTICAL)
+        text = wx.StaticText(self, label="Local Directory", style=wx.ALIGN_CENTRE)
+        box.Add(text, 0, wx.EXPAND | wx.ALL, 15)
+        self.SetSizer(box);
+        self.SetBackgroundColour('#FF4136') #red
+
+class RemoteDirPanel(wx.Panel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        box = wx.BoxSizer(wx.VERTICAL)
+        text = wx.StaticText(self, label="Remote Directory", style=wx.ALIGN_CENTRE)
+        box.Add(text, 0, wx.EXPAND | wx.ALL, 15)
+        self.SetSizer(box);
+        self.SetBackgroundColour('#85144b') #maroon
+        
+class MainFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super(MainFrame, self).__init__(parent, title=title)
+        self.CreateUI()
+
+    def CreateUI(self):
+        panel = wx.Panel(self)
+        panel.SetBackgroundColour('#ffffff')
+
+        rows = wx.BoxSizer(wx.VERTICAL)
+        rows.Add(ToolbarPanel(panel), 0, wx.EXPAND)
+        rows.Add(ConsolePanel(panel), 0, wx.EXPAND)
+        dirs = wx.BoxSizer(wx.HORIZONTAL)
+        dirs.Add(LocalDirPanel(panel), 1, wx.EXPAND)
+        dirs.Add(RemoteDirPanel(panel), 1, wx.EXPAND) 
+        rows.Add(dirs, 1, wx.EXPAND)
+        panel.SetSizer(rows)
+        
+
+if __name__ == '__main__':
+    app = wx.App(False)
+    frame = MainFrame(None, title='FTP Client')
+    frame.Show()
+    app.MainLoop()
+
