22c99aec4130ff38b2073a001671e7ac24208412
Jaakko Uusitalo <jaakkouusitalo@outlook.com>
2020-02-02 20:55:41 +0200

    added abort connection button and ftp messages

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 from ftplib import FTP
 import wx
+import wx.lib.inspection
 
-def Connect(url, user, passwd):
+global ftp
+
+def Connect(self, url, user, passwd):
+    global ftp
     ftp = FTP(url)
     ftp.login(user, passwd)
-    wx.MessageBox(ftp.getwelcome())
+    wx.MessageBox(ftp.getwelcome(), "Connection Success", wx.ICON_INFORMATION)
+    self.connectFtpBtn.Hide()
+    self.abortConnBtn.Show()
+    self.Layout()
 
 class ToolbarPanel(wx.Panel):
     def __init__(self, parent):
         super().__init__(parent)
         box = wx.BoxSizer(wx.HORIZONTAL)
-        ftpUserText = wx.StaticText(self, -1, "User:")
-        ftpPassText = wx.StaticText(self, -1, "Pass:")
-        ftpUrlText = wx.StaticText(self, -1, "Url:")
+        ftpUserText = wx.StaticText(self, -1, "User :")
+        ftpPassText = wx.StaticText(self, -1, "Pass :")
+        ftpUrlText = wx.StaticText(self, -1, "Url :")
         self.ftpUserCtrl = wx.TextCtrl(self, -1) 
         self.ftpPassCtrl = wx.TextCtrl(self, -1, style=wx.TE_PASSWORD)
         self.ftpUrlCtrl = wx.TextCtrl(self, -1)
-        connectFtpBtn = wx.Button(self, -1, "Connect")
-        connectFtpBtn.Bind(wx.EVT_BUTTON, self.OnConnectFtpBtnClick)
+        self.connectFtpBtn = wx.Button(self, -1, "Connect")
+        self.abortConnBtn = wx.Button(self, -1, "Abort Connection")
+        self.abortConnBtn.Hide()
+        self.connectFtpBtn.Bind(wx.EVT_BUTTON, self.OnConnectFtpBtnClick)
+        self.abortConnBtn.Bind(wx.EVT_BUTTON, self.OnAbortConnBtnClick)
         box.Add(ftpUserText, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
         box.Add(self.ftpUserCtrl, 0, wx.EXPAND | wx.ALL, 5)
         box.Add(ftpPassText, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
         box.Add(self.ftpPassCtrl, 0, wx.EXPAND | wx.ALL, 5)
         box.Add(ftpUrlText, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
         box.Add(self.ftpUrlCtrl, 0, wx.EXPAND | wx.ALL, 5)
-        box.Add(connectFtpBtn, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(self.connectFtpBtn, 0, wx.EXPAND | wx.ALL, 5)
+        box.Add(self.abortConnBtn, 0, wx.EXPAND | wx.ALL, 5)
         self.SetSizer(box)
         self.SetBackgroundColour('#ececec')
     
@@ -32,7 +43,22 @@ class ToolbarPanel(wx.Panel):
         username = self.ftpUserCtrl.GetValue()
         password = self.ftpPassCtrl.GetValue()
         url = self.ftpUrlCtrl.GetValue()
-        Connect(url, username, password)
+        if len(username) == 0:
+            wx.MessageBox("Please provide username", "Connection Error", wx.ICON_ERROR)
+        elif len(password) == 0:
+            wx.MessageBox("Please provide password", "Connection Error", wx.ICON_ERROR)
+        elif len(url) == 0:
+            wx.MessageBox("Please provide url", "Connection Error", wx.ICON_ERROR)
+        else:
+            Connect(self, url, username, password)
+    
+    def OnAbortConnBtnClick(self, e):
+        global ftp
+        ftp.quit()
+        wx.MessageBox("Connection has been closed successfully!", "Connection Closed", wx.ICON_INFORMATION)
+        self.connectFtpBtn.Show()
+        self.abortConnBtn.Hide()
+        self.Layout()
 
 class ConsolePanel(wx.Panel):
     def __init__(self, parent):
@@ -62,6 +88,7 @@ class RemoteDirPanel(wx.Panel):
 class MainFrame(wx.Frame):
     def __init__(self, parent, title):
         super(MainFrame, self).__init__(parent, title=title, size=wx.Size(900, 500))
+        self.ftp = None
         self.CreateUI()
 
     def CreateUI(self):
@@ -78,6 +105,7 @@ class MainFrame(wx.Frame):
         
 if __name__ == '__main__':
     app = wx.App(False)
+    wx.lib.inspection.InspectionTool().Show()
     frame = MainFrame(None, title='FTP Client')
     frame.Show()
     app.MainLoop()
